4d9a2cda4555d4048aded1ba21a472302b82a2c2
joepaint <72581619+joepaint@users.noreply.github.com>
2023-11-19 16:13:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import * as YUKA from 'yuka';
+import * as THREE from './node_modules/three/build/three.module.js';
+//import * as YUKA from 'yuka';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
 
