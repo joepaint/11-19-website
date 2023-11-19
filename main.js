ffcf738434d8749b8c57ffc10bfa434aab7cf08a
joepaint <72581619+joepaint@users.noreply.github.com>
2023-11-19 17:32:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import mimetypes
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
+
 import * as THREE from './node_modules/three/build/three.module.js';
 //import * as YUKA from 'yuka';
 import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
